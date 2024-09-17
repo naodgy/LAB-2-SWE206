9ee9ef664bab01873a5e4a04ff5ce6e983fe4b43
Baleid <165569926+1Baleid@users.noreply.github.com>
2024-09-17 19:24:50 +0300

    I added a new print statemnt

---
+++ NewApp.java
@@ -0,0 +1,8 @@
+public class NewApp {
+    public static void main(String[] args) {
+        // Your application logic goes here
+        System.out.println("Hello, world!");
+
+        System.out.println("Adding new print statmnents.")
+    }
+}
